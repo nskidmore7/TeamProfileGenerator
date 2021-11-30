3ae62e9c82ba99a1f6dfaef13526bd51232019d6
nskidmore7 <njskid11@gmail.com>
2021-11-30 11:54:39 -0600

    fixing file

--- index.js
+++ index.js
@@ -3,34 +3,67 @@ const fs = require("fs");
 const manager = require("./lib/manager");
 const engineer = require("./lib/engineer");
 const intern = require("./lib/intern");
-const pgTemplate = require("./src/page-template");
-const { COMPLETIONSTATEMENT_TYPES } = require("@babel/types");
 const pageTemplate = require("./src/page-template");
 
-const managerQuestions = [
-    {
-        type: "input",
-        message: "What is the Manager's name?",
-        name: "managerName",
-    },
-    {
-        type: "input",
-        message: "What is the Manager's email?",
-        name: "managerEmail",
-    },
-    {
-        type: "input",
-        message: "What is the Manager's phone number?",
-        name: "managerNumber",
-    },
-    {
-        type: "input",
-        message: "What is the Manager's employee ID?",
-        name: "managerID",
-    },
-];
-const engineerQuestions = [
-    {
+const team = [];
+function addManager() {
+    function managerQuestions () {
+        inquirer
+        .prompt([
+        {
+            type: "input",
+            message: "What is the Manager's name?",
+            name: "managerName",
+        },
+        {
+            type: "input",
+            message: "What is the Manager's email?",
+            name: "managerEmail",
+        },
+        {
+            type: "input",
+            message: "What is the Manager's phone number?",
+            name: "managerNumber",
+        },
+        {
+            type: "input",
+            message: "What is the Manager's employee ID?",
+            name: "managerID",
+        }
+    ])
+    .then(Response => {
+        team.push (new manager(Response.managerName, Response.managerid, Response.manageremail, Response.managerNumber))
+        addMembers();
+    })
+}
+
+function addMembers() {
+    inquirer
+    .prompt([
+        {
+            type: "list",
+        name: "addMemberList",
+        message: "Would you like to add an additional member to your team? If so select their role. If not select done.",
+        choices: ["engineer", "intern", "done"]
+        }
+    ])
+
+    .then(function(Response) {
+        if(Response.role === "engineer") {
+            engineerQuestions();
+        }
+        else if(Response.role === "intern") {
+            internQuestions();
+        }
+        else if(Response.role === "done") {
+            createHTML();
+        }
+    })
+}
+function engineerQuestions ()   {
+    inquirer
+    .prompt([
+  {
         type: "input",
         message: "What is the Engineer's name?",
         name: "engineerName",
@@ -49,9 +82,18 @@ const engineerQuestions = [
         type: "input",
         message: "What is the Engineer's employee ID?",
         name: "engineerID",
-    },
-];
-const internQuestions = [
+    }
+])
+.then(Response => {
+team.push (new engineer(Response.engineerName, Response.engineerEmail, Response.engineerGithub, Response.engineerID))
+addMembers();
+})
+}
+
+
+function internQuestions ()   {
+    inquirer
+    .prompt([
     {
         type: "input",
         message: "What is the Intern's name?",
@@ -72,87 +114,18 @@ const internQuestions = [
         message: "What is the Intern's employee ID?",
         name: "internID",
     },
-];
-
-const addMembers = [
-    {
-        type: "list",
-        name: "addMemberList",
-        message: "Would you like to add an additional member to your team? If so select their role. If not select done.",
-        choices: ["engineer", "intern", "done"],
-    },
-];
-
-const team = [];
-const init = () => {
-    inquirer
-        .prompt(managerQuestions)
-        .then((r) => {
-            let newManager = new manager (
-                r.managerName,
-                r.managerEmail,
-                r.managerNumber,
-                r.managerID
-            );
-                team.push(newManager);
-        })
-        .then(() => {
-        addMemInit();
+])
+.then(Response => {
+    team.push (new intern(Response.internName, Response.internEmail, Response.internGithub, Response.internID))
+    addMembers();
     })
-    .catch(() => {
-        console.log("Try Again");
-    });
-};
+    }
+  managerQuestions();
+}
+    
 
-const addMemInit = () => {
-    inquirer
-        .prompt(addMembers)
-        .then((response) => {
-            if (response.addMemSelect === "engineer") {
-                inquirer
-                .prompt(engineerQuestions)
-                .then((r) => {
-                    let newEngineer = new engineer (
-                        r.engineerName,
-                        r.engineerEmail,
-                        r.engineerGithub,
-                        r.engineerID
-                    );
-                    team.push(newEngineer);
-                    addMemInit();
-                })
-                .catch();
-            }
-            if (response.addMemSelect === "intern") {
-                inquirer
-                .prompt(internQuestions)
-                .then((r) => {
-                    let newIntern = new intern (
-                        r.internName,
-                        r.internEmail,
-                        r.internSchool,
-                        r.internID
-                    );
-                    team.push(newIntern);
-                    addMemInit();
-                })
-                .catch();
-            }
-            if (response.addMemSelect === "done") {
-                finish(team);
-            }
-        })
-        .catch(() => {
-            console.log("Try Again");
-        });
-};
+function createHTML() {
+    fs.writeFile("team-profile.html", pageTemplate(team), (err) => err ? console.log(err):console.log('complete'))
+}
 
-const finish = (team) => {
-    fs.writeFile("./dist/team.html", pageTemplate(team), (err) => {
-        if (err) {
-            return console.error(err);
-        }
-    });
-};
-
-init();
+addManager();
